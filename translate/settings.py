f37e5b80ab1b1a6845c750717cd19f7c2884ce60
Jassim Alhatem <duod2003row@gmail.com>
2021-07-25 18:41:55 +0300

    Added a Home Page

--- translate/settings.py
+++ translate/settings.py
@@ -15,7 +15,6 @@ import psycopg2
 import dj_database_url
 
 from pathlib import Path
-from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
