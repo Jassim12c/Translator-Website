a493628912651bbc795c179a130799c5cc08ce82
Jassim Alhatem <duod2003row@gmail.com>
2021-09-17 08:13:52 +0300

    Added new allowed host

--- translate/settings.py
+++ translate/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "PASS")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['translatetoarabic.herokuapp.com/', 'localhost']
 
 
 # Application definition
